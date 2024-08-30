4044697878b1cc1fd264194fd126f583eacdd099
Ramandeep03 <s.raman03@hotmail.com>
2024-08-30 20:42:54 +0530

    Initial Commit

---
+++ story/storyApp.swift
@@ -0,0 +1,17 @@
+//
+//  storyApp.swift
+//  story
+//
+//  Created by Ramandeep Singh on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct storyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
